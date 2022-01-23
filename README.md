3323a353b942baf3f7877e0c36a862e0c47f3cf9
Yasmin Ateya <36104822+yasminateya@users.noreply.github.com>
2022-01-23 21:56:40 +0100

    feat: add interacting web elements file

---
+++ README.md
@@ -0,0 +1,27 @@
+## Cypress repo
+This repo for automating local website [ngx-admin] using JavaScript and Cypress
+
+### How to run:
+1. Install application under test from this repo [ngx-admin](https://github.com/Postavshik/ngx-cypress-test)  
+2. Open the project 
+3. Run from the terminal 
+```bash
+npm install or npm install --legacy-peer-deps
+```
+4. Run this command then open the url
+```bash
+npm start
+```
+5. Clone my repo [Cypress-repo](https://github.com/yasminateya/Cypress-repo) 
+6. Install Cypress via npm
+```bash
+npm install cypress --save-dev
+```
+7. Open cypress runner to run automated test cases and show the running
+```bash
+npx cypress open
+```
+8. Use this command if you want to run it in a headless mode
+```bash
+npx cypress run
+```
---
+++ cypress/integration/web-elements-interacting.spec.js
@@ -0,0 +1,65 @@
+/// <reference types="cypress" />
+
+describe('Section 3: web elements', () => {
+
+    beforeEach('before', () => {
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+    })
+
+    //reference : Section 3: Interaction with Web Elements
+        
+        //         To get locators:
+        // 1. Tag name >> cy.get('value')
+        // 2. Id >> cy.get('#value')
+        // 3. Class name >> cy.get('.value')
+        // 4. Attribute name >> cy.get([value])
+        // 5. Attribute name and value >> cy.get([value="value"])  
+        // 6. Cypress >> cy.get('[data-cy=login-button]').click()
+
+        // find() : to find child element inside the parent
+        // parents() : to locate the parents elements from the current key element which you are in
+        // then() : to save the contain value on new method .. it has jquery type
+        // wrap() : If we want to return parameter back from jquery to cypress format
+        // invoke() : if we want to get specific thing (text, attribute value ..) .. it has jquery type
+
+
+    it('check then and wrap commands', () => {
+        // we will travel from parent(label using grid "nb-card") to child (email label)
+        // cy.contains('nb-card', 'Using the Grid') = get (nb-card) firstly which contains 'Using the Grid'
+        // save the value of contains using then command inside the parameter "firstForm"
+        // The parameter is jquery object not cypress object so you need to use expect not should, this find is jquery as well
+
+        cy.contains('nb-card', 'Using the Grid').then( firstForm => {
+            const emailLabelFirst = firstForm.find('[for="inputEmail1"]').text()
+            const passwordLabelFirst = firstForm.find('[for="inputPassword2"]').text()
+            expect(emailLabelFirst).equal('Email')
+            expect(passwordLabelFirst).equal('Password')
+
+            cy.contains('nb-card', 'Basic form').then( secondForm => {
+                const passwordLabelSecond = secondForm.find('[for="exampleInputPassword1"]').text()
+                expect(passwordLabelFirst).equal(passwordLabelSecond)
+
+                cy.wrap(secondForm).find('[for="exampleInputPassword1"]').should('contain', 'Password')
+            })
+        })
+    })
+
+    it.only('check invoke command', () => {
+        cy.get('[for="exampleInputEmail1"]').invoke('text').then( text => {
+            expect(text).equal('Email address')
+        })
+
+        // check if check box in Basic form if it is checked or not after clicking
+        cy.contains('nb-card', 'Basic form')
+        .find('nb-checkbox')
+        .click()
+        .find('.custom-checkbox')
+        .invoke('attr','class') 
+        .should('contain', 'checked')
+        // we are trying to invoke the value from attrubite with type class [class="custom-checkbox checked"]
+
+    })
+
+})
