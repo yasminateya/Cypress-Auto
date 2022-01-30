de8d2dfeb1ce972a08a0fd43a843626c20e9afc7
Yasmin Ateya <36104822+yasminateya@users.noreply.github.com>
2022-01-30 10:11:24 +0100

    fix: date method

--- cypress/integration/web-elements-interacting.spec.js
+++ cypress/integration/web-elements-interacting.spec.js
@@ -2,12 +2,6 @@
 
 describe('Section 3: web elements', () => {
 
-    // beforeEach('before', () => {
-    //     cy.visit('/')
-    //     cy.contains('Forms').click()
-    //     cy.contains('Form Layouts').click()
-    // })
-
     //reference : Section 3: Interaction with Web Elements
 
 
@@ -23,16 +17,16 @@ describe('Section 3: web elements', () => {
     // find() : to find child element inside the parent
     // parents() : to locate the parents elements from the current key element which you are in
     // then() : to save the contain value on new method .. it has jquery type
-    // wrap() : If we want to return parameter back from jquery to cypress format
-    // invoke() : if we want to get specific thing (text, attribute value ..)
+    // wrap() : to return parameter back from jquery to cypress format
+    // invoke() : to get specific thing (text, attribute value ..)
     // check() : to deal with check boxes and radio buttons
-    // select() : we can use it if we have <select> tag but here we don't have it
+    // select() : if I <select> tag but here I don't have it
 
     /********************** 12. Saving subject of the command ***********************/
-    it('check then and wrap commands', () => {
+    it('1. check then and wrap commands', () => {
 
         cy.openFormLayoutsPage()
-        // we will travel from parent(label using grid "nb-card") to child (email label)
+        // i will travel from parent(label using grid "nb-card") to child (email label)
         // cy.contains('nb-card', 'Using the Grid') = get (nb-card) firstly which contains 'Using the Grid'
         // save the value of contains using then command inside the parameter "firstForm"
         // The parameter is jquery object not cypress object so you need to use expect not should, this find is jquery as well
@@ -53,7 +47,7 @@ describe('Section 3: web elements', () => {
     })
 
     /********************** 13. Invoke command ***********************/
-    it('check invoke command', () => {
+    it('2. check invoke command', () => {
 
         cy.openFormLayoutsPage()
 
@@ -68,27 +62,46 @@ describe('Section 3: web elements', () => {
             .find('.custom-checkbox')
             .invoke('attr', 'class')
             .should('contain', 'checked')
-        // we are trying to invoke the value from attrubite with type class [class="custom-checkbox checked"]
+        // i am trying to invoke the value from attrubite with type class [class="custom-checkbox checked"]
 
     })
 
-    it('assert property with invoke command', () => {
+    /********************** 17. Web Datepickers ***********************/
+    // in test 3 I used hard code date so I will use Date interface to select date depend on the current date
+    it('3. assert property with invoke command', () => {
 
         cy.openDatePickerPage()
         // sometimes, you can't find the value on the elemnts so check the properties tab
-        // we need to get the date value 
+        // i need to get the date value 
+
+        //get current date and month (short month DEC with toLocaleDateString())
+        let date = new Date()
+        date.setDate(date.getDate() + 1)
+        let nextDate =  date.getDate()
+        console.log(nextDate)
+        let currentMonth = date.toLocaleDateString('default', {month: 'short'})
 
         cy.contains('nb-card', 'Common Datepicker').find('input').then(input => {
             cy.wrap(input).click()
-            // select the locator (parent) for the whole calendar not a specific date
-            cy.get('nb-calendar-day-picker').contains('4').click()
-            // search on properties for the selected date
-            cy.wrap(input).invoke('prop', 'value').should('contain', 'Jan 4, 2022')
+
+            cy.get('nb-calendar-navigation').invoke('attr', 'ng-reflect-date').then(actualDate => {
+                if (!actualDate.includes(currentMonth)){
+                    cy.get('[data-name="chevron-right"]').click()
+                    cy.get('nb-calendar-day-picker [class="day-cell ng-star-inserted"]').contains(nextDate).click()
+                } else {
+                    cy.get('nb-calendar-day-picker [class="day-cell ng-star-inserted"]').contains(nextDate).click()
+                }
+            })
+
+            // // select the locator (parent) for the whole calendar not a specific date
+            // cy.get('nb-calendar-day-picker').contains('4').click()
+            // // search on properties for the selected date
+            // cy.wrap(input).invoke('prop', 'value').should('contain', 'Jan 4, 2022')
         })
     })
 
     /********************** 14. Checkboxes and Radio buutons ***********************/
-    it('assert first radio button selected then select second one and check first is unselected', () => {
+    it('4. assert first radio button selected then select second one and check first is unselected', () => {
 
         cy.openFormLayoutsPage()
 
@@ -114,9 +127,9 @@ describe('Section 3: web elements', () => {
 
     })
 
-    it('test checkboxes in Toastr page', () => {
+    it('5. test checkboxes in Toastr page', () => {
         cy.openToastrPage()
-        // we have index 0, 2 checked by defult
+        // i have index 0, 2 checked by defult
         // check method will check all checkboxes with the same attribute.. if there is one already checked it will not uncheck it
         // if you want to uncheck just replace check() with click() or uncheck()
         cy.get('[type="checkbox"]').eq(0).should('be.checked')
@@ -128,15 +141,15 @@ describe('Section 3: web elements', () => {
     })
 
     /********************** 15. Lists and Dropdowns ***********************/
-    it('check dark mode is applied correctly after selecting it', () => {
+    it('6. check dark mode is applied correctly after selecting it', () => {
         cy.visit('/')
         /****************** way 1 ****************/
-        // // click on modes - we have parent tag "nav" and child tag "nb-select" so we can add them together
+        // // click on modes - i have parent tag "nav" and child tag "nb-select" so i can add them together
         // cy.get('nav nb-select').click()
         // // use locator for the whole dropdowns
         // cy.get('.options-list').contains('Dark').click()
 
-        // // we need to check the background, it is css value or attr, you can find it on the Styles
+        // // i need to check the background, it is css value or attr, you can find it on the Styles
         // // color in the styles with HEX format and Cypress accepts RGP sp just change it
         // //you can find '.nb-theme-dark nb-layout-header nav' so select header+ tag name 'nb-layout-header nav'
         // cy.get('nb-layout-header nav').should('have.css', 'background-color', 'rgb(34, 43, 69)')
@@ -146,7 +159,7 @@ describe('Section 3: web elements', () => {
 
         /****************** way 2 ****************/
 
-        // if we want to select each mode and assert that is selected correctly, it will be hassle to repeat our code manytimes so use then() to create variable and loop through the dropdown
+        // if i want to select each mode and assert that is selected correctly, it will be hassle to repeat our code manytimes so use then() to create variable and loop through the dropdown
         cy.get('nav nb-select').then(modeButton => {
             cy.wrap(modeButton).click()
             // class + tag name for all list
@@ -175,8 +188,8 @@ describe('Section 3: web elements', () => {
 
     /********************** 16. Web Tables ***********************/
     // 1. table body"tbody" >> 2. table row "tr" >> 3. table coloumn "td"
-    it('check age of Larry is saved correctly after editing', () => {
-        // scenario 1: we need to find row of Larry then edit his age
+    it('7. check age of Larry is saved correctly after editing', () => {
+        // scenario 1: i need to find row of Larry then edit his age
         const newAge = '25'
         cy.openSmartTablePage()
         cy.get('tbody').contains('tr', 'Larry').then(larryRow => {
@@ -189,8 +202,8 @@ describe('Section 3: web elements', () => {
 
     })
 
-    // scenario 2: we need to add new entry
-    it('check data is added correctly after adding new row', () => {
+    // scenario 2: i need to add new entry
+    it('8. check data is added correctly after adding new row', () => {
         const firstName = 'Yasmin'
         const lastName = 'Ateya'
         cy.openSmartTablePage()
@@ -208,7 +221,7 @@ describe('Section 3: web elements', () => {
 
 
     // scenario 3: search for 20 in age field and check it is retuned all rows with 20
-    it.only('check data is returned correctly after searching for specific age', () => {
+    it('9. check data is returned correctly after searching for specific age', () => {
         const age = ['20', '30', '40']
 
         cy.wrap(age).each(age => {
